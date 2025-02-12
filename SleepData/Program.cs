d29d870200b548b73d47339051564c17daff493d
Hardfax <jpodrug@my.wctc.edu>
2025-02-12 16:00:38 -0600

    Add files via upload

---
+++ SleepData/Program.cs
@@ -0,0 +1,51 @@
+﻿// ask for input
+using System.Text;
+
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();  
+
+if (resp == "1")
+{
+    // create data file
+
+    // ask a question
+    Console.WriteLine("How many weeks of data is needed?");
+    // input the response (convert to int)
+    int weeks = Convert.ToInt32(Console.ReadLine());
+    // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    // random number generator
+    Random rnd = new();
+    // create file
+    StreamWriter sw = new("data.txt");
+
+    // loop for the desired # of weeks
+    while (dataDate < dataEndDate)
+    {
+        // 7 days in a week
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
+        {
+            // generate random number of hours slept between 4-12 (inclusive)
+            hours[i] = rnd.Next(4, 13);
+        }
+        // M/d/yyyy,#|#|#|#|#|#|#
+                // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        Console.WriteLine($"Week of {dataDate:MMM}, {dataDate:dd}, {dataDate:yyyy}\r Su Mo Tu We Th Fr Sa\r -- -- -- -- -- -- --\r {string.Join(" ", hours)}");
+        // add 1 week to date
+        dataDate = dataDate.AddDays(7);
+    }
+    sw.Close();
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+
+}
